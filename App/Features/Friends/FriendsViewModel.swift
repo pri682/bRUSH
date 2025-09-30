6b7f092a8b5ae2f90abeccb44e0f410aa784f6d9
Jesse Flynn <bsw80@txstate.edu>
2025-09-29 21:33:40 -0500

    KAN-18 add FriendsViewModel with @Published state

--- App/Features/Friends/FriendsViewModel.swift
+++ App/Features/Friends/FriendsViewModel.swift
@@ -1,7 +1,17 @@
-//
-//  FriendsViewModel.swift
-//  DoodleOver
-//
-//  Created by Meidad Troper on 9/25/25.
-//
+import Foundation
+import SwiftUI
+import Combine
+
+@MainActor
+class FriendsViewModel: ObservableObject {
+    @Published var friends: [Friend] = []
+    
+    func loadMock() {
+        friends = [
+            Friend(name: "Ted", handle: "@grumpyoldman"),
+            Friend(name: "Aaron", handle: "@lunchalone"),
+            Friend(name: "Jeffrey", handle: "@dahmer")
+        ]
+    }
+}
 
