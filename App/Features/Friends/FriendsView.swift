cccea548dfdb3755eeb959282918ea3af8c0655e
Jesse Flynn <bsw80@txstate.edu>
2025-09-29 21:33:40 -0500

    KAN-18 add FriendsViewModel with @Published state

--- App/Features/Friends/FriendsView.swift
+++ App/Features/Friends/FriendsView.swift
@@ -1,19 +1,16 @@
 import SwiftUI
 
 struct FriendsView: View {
-    let mockFriends = [
-        Friend(name: "Ted", handle: "@grumpyoldman"),
-        Friend(name: "Aaron", handle: "@lunchalone"),
-        Friend(name: "Jeffrey", handle: "@dahmer")
-    ]
+    @StateObject private var vm = FriendsViewModel()
     
     var body: some View {
-        List(mockFriends) { friend in
+        List(vm.friends) { friend in
             VStack(alignment: .leading) {
                 Text(friend.name).font(.headline)
                 Text(friend.handle).foregroundColor(.secondary)
             }
         }
         .navigationTitle("Friends")
+        .onAppear { vm.loadMock() }
     }
 }
