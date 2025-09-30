70ab2c79000a6906a897f11f41f2631015b677af
Jesse Flynn <bsw80@txstate.edu>
2025-09-29 21:26:57 -0500

    KAN-18 add Friend model and sample data

--- App/Features/Friends/FriendsView.swift
+++ App/Features/Friends/FriendsView.swift
@@ -1,8 +1,19 @@
 import SwiftUI
 
 struct FriendsView: View {
+    let mockFriends = [
+        Friend(name: "Ted", handle: "@grumpyoldman"),
+        Friend(name: "Aaron", handle: "@lunchalone"),
+        Friend(name: "Jeffrey", handle: "@dahmer")
+    ]
+    
     var body: some View {
-        Text("Your Friends List")
-            .font(.largeTitle)
+        List(mockFriends) { friend in
+            VStack(alignment: .leading) {
+                Text(friend.name).font(.headline)
+                Text(friend.handle).foregroundColor(.secondary)
+            }
+        }
+        .navigationTitle("Friends")
     }
 }
