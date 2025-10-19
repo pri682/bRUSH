c74431351d92b9d835e41941615b24467d7887a6
Kelvin Mathew <kelvinmathew2004@gmail.com>
2025-10-19 18:22:15 -0500

    Implemented custom UI to select multiple drawings to delete KAN-140

--- App/Features/Drawings/GridItemView.swift
+++ App/Features/Drawings/GridItemView.swift
@@ -5,6 +5,7 @@ struct GridItemView: View {
     let namespace: Namespace.ID
     let size: Double
     let item: Item
+    var isSelected: Bool = false
     
     var body: some View {
         ZStack {
@@ -19,7 +20,17 @@ struct GridItemView: View {
         .frame(width: size, height: size * (16 / 9))
         .clipShape(.rect(cornerRadius: 8.0))
         .matchedGeometryEffect(id: item.id, in: namespace)
-        .shadow(radius: 5)
+        .overlay {
+            if isSelected {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 8.0)
+                        .fill(Color.black.opacity(0.4))
+                    Image(systemName: "checkmark.circle.fill")
+                        .foregroundColor(.accentColor)
+                        .font(.title)
+                }
+            }
+        }
         .onAppear {
             if item.image == nil {
                 dataModel.loadImage(for: item.id)
@@ -27,4 +38,3 @@ struct GridItemView: View {
         }
     }
 }
-
