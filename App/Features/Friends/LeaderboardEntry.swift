394fa27c159a40e50f3154de4e55504e6ea045dc
Jesse Flynn <bsw80@txstate.edu>
2025-10-21 16:12:38 -0500

    KAN-104 increased medal point multipliers to feel more rewarding
    Gold = 100 points
    Silver = 25 points
    Bronze = 10 points

--- App/Features/Friends/LeaderboardEntry.swift
+++ App/Features/Friends/LeaderboardEntry.swift
@@ -10,7 +10,8 @@ struct LeaderboardEntry: Identifiable, Hashable {
     let bronze: Int
     let submittedAt: Date   // tie breaker
     
-    var points: Int { gold * 3 + silver * 2 + bronze }
+    // increased multipliers to feel more rewarding
+    var points: Int { gold * 100 + silver * 25 + bronze * 10}
     
     init(
         uid: String,
