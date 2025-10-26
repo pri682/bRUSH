8cd501df749096b26d389ef9650b1df9b7ef2ad4
Vaidic Soni <wse11@txstate.edu>
2025-10-26 16:42:30 -0500

    Fixed HomeView

--- App/Features/Home/HomeView.swift
+++ App/Features/Home/HomeView.swift
@@ -242,6 +242,10 @@ struct HomeView: View {
         }
         .task {
             await viewModel.loadDailyPrompt()
+            await viewModel.loadFeed()
+        }
+        .refreshable {
+            await viewModel.loadFeed()
         }
     }
 
