e92caa074f38ffdab7a2e14c04ab49855c493d74
Jesse Flynn <bsw80@txstate.edu>
2025-10-26 13:27:19 -0500

    KAN-107 Add remove locally test

--- brushTests/brushTests.swift
+++ brushTests/brushTests.swift
@@ -21,4 +21,21 @@ final class FriendsViewModelTests: XCTestCase {
         vm.searchText = ""
         XCTAssertEqual(vm.filteredFriends.count, 3)
     }
+    
+    @MainActor
+    func testRemoveLocally_RemovesFriendsAndIds() {
+        let vm = FriendsViewModel()
+        vm.friends = [
+            Friend(uid: "a", name: "A", handle: "@a"),
+            Friend(uid: "b", name: "B", handle: "@b"),
+            Friend(uid: "c", name: "C", handle: "@c"),
+        ]
+        vm.friendIds = ["a", "b", "c"]
+        vm.addQuery = "" // so performAddSearch() won’t be called
+
+        vm.removeLocally(uids: ["b", "c"])
+        XCTAssertEqual(vm.friends.map(\.uid), ["a"])
+        XCTAssertEqual(vm.friendIds, ["a"])
+    }
+    
 }
