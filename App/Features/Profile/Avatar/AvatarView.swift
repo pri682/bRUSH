cac0fa2b725d534a506739d7db080dde9f38d33d
Meidad Troper <oro27@txstate.edu>
2025-11-11 00:31:09 -0600

    KAN-145 Added Spacer On Top
    Added spacer on top of the avatar stack so the floating island doesn't overlap the avatar

--- App/Features/Profile/Avatar/AvatarView.swift
+++ App/Features/Profile/Avatar/AvatarView.swift
@@ -1,7 +1,12 @@
 import SwiftUI
+import UIKit // Keep UIKit for UIDevice and the renderAsImage extension
+
+// **NOTE:** The AvatarType enum is expected to be defined elsewhere in your project.
+// We are removing the minimal definition that was causing the 'redeclaration' error.
 
 struct AvatarView: View {
-    let avatarType: AvatarType
+    // Keep your original properties
+    let avatarType: AvatarType // Ensure AvatarType is accessible (e.g., in a shared file)
     let background: String
     let avatarBody: String?
     let shirt: String?
@@ -15,72 +20,84 @@ struct AvatarView: View {
                 .resizable()
                 .scaledToFill()
             
-            if avatarType == .personal {
-                // Personal avatar layers (human)
-                // Body layer (if exists)
-                if let avatarBody = avatarBody {
-                    Image(avatarBody)
-                        .resizable()
-                        .scaledToFit()
+            VStack {
+                // Conditional spacer for iPhone devices
+                if UIDevice.current.userInterfaceIdiom == .phone {
+                    // This spacer pushes the ZStack down on iPhones
+                    Color.clear
+                        .frame(height: 65)
                 }
                 
-                // Shirt layer (if exists)
-                if let shirt = shirt {
-                    Image(shirt)
-                        .resizable()
-                        .scaledToFit()
-                }
-                
-                // Eyes layer (if exists)
-                if let eyes = eyes {
-                    Image(eyes)
-                        .resizable()
-                        .scaledToFit()
-                }
-                
-                // Mouth layer (if exists)
-                if let mouth = mouth {
-                    Image(mouth)
-                        .resizable()
-                        .scaledToFit()
-                }
-                
-                // Hair layer (top, if exists)
-                if let hair = hair {
-                    Image(hair)
-                        .resizable()
-                        .scaledToFit()
-                }
-            } else {
-                // Fun avatar layers (alien) - face, eyes, mouth, hair
-                // Face layer (if exists) - mapped from body
-                if let face = avatarBody {
-                    Image(face)
-                        .resizable()
-                        .scaledToFit()
-                }
-                
-                // Eyes layer (if exists)
-                if let eyes = eyes {
-                    Image(eyes)
-                        .resizable()
-                        .scaledToFit()
-                }
-                
-                // Mouth layer (if exists)
-                if let mouth = mouth {
-                    Image(mouth)
-                        .resizable()
-                        .scaledToFit()
-                }
-                
-                // Hair layer (top, if exists)
-                if let hair = hair {
-                    Image(hair)
-                        .resizable()
-                        .scaledToFit()
-                }
-            }
+                // Group all avatar elements (Personal or Fun)
+                ZStack {
+                    if avatarType == .personal {
+                        // Personal avatar layers (human)
+                        // Body layer
+                        if let avatarBody = avatarBody {
+                            Image(avatarBody)
+                                .resizable()
+                                .scaledToFit()
+                        }
+                        
+                        // Shirt layer (on top of body)
+                        if let shirt = shirt {
+                            Image(shirt)
+                                .resizable()
+                                .scaledToFit()
+                        }
+                        
+                        // Eyes layer
+                        if let eyes = eyes {
+                            Image(eyes)
+                                .resizable()
+                                .scaledToFit()
+                        }
+                        
+                        // Mouth layer
+                        if let mouth = mouth {
+                            Image(mouth)
+                                .resizable()
+                                .scaledToFit()
+                        }
+                        
+                        // Hair layer (topmost)
+                        if let hair = hair {
+                            Image(hair)
+                                .resizable()
+                                .scaledToFit()
+                        }
+                    } else {
+                        // Fun avatar layers (alien) - mapping avatarBody to face
+                        // Face layer
+                        if let face = avatarBody {
+                            Image(face)
+                                .resizable()
+                                .scaledToFit()
+                        }
+                        
+                        // Eyes layer
+                        if let eyes = eyes {
+                            Image(eyes)
+                                .resizable()
+                                .scaledToFit()
+                        }
+                        
+                        // Mouth layer
+                        if let mouth = mouth {
+                            Image(mouth)
+                                .resizable()
+                                .scaledToFit()
+                        }
+                        
+                        // Hair layer (topmost)
+                        if let hair = hair {
+                            Image(hair)
+                                .resizable()
+                                .scaledToFit()
+                        }
+                    }
+                } // End inner ZStack for avatar parts
+            } // End VStack for conditional spacing
         }
         .clipped()
     }
@@ -90,6 +107,7 @@ struct AvatarView: View {
 extension AvatarView {
     // render a 200 x 200 image of anything. calling avatar.renderimage will render the avatar as image:
     func renderAsImage(size: CGSize = CGSize(width: 200, height: 200)) -> UIImage? {
+        // We use ImageRenderer and UIScreen which require 'import SwiftUI' and 'import UIKit'
         let renderer = ImageRenderer(content: self.frame(width: size.width, height: size.height))
         renderer.scale = UIScreen.main.scale
         return renderer.uiImage // returns the image
@@ -97,6 +115,7 @@ extension AvatarView {
 }
 
 // Preview for development
+// Ensure this is treated as a SwiftUI file for the PreviewProvider to work
 struct AvatarView_Previews: PreviewProvider {
     static var previews: some View {
         AvatarView(
@@ -109,6 +128,6 @@ struct AvatarView_Previews: PreviewProvider {
             hair: "hair_1"
         )
         .frame(width: 200, height: 200)
-        .previewLayout(.sizeThatFits)
+        .previewLayout(.sizeThatFits) // This should now resolve
     }
 }
