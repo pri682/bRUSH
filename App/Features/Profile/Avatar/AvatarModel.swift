f99536c065ef99aeb266a6ebe7f4eeb5582d8f2e
Meidad Troper <oro27@txstate.edu>
2025-11-13 21:45:32 -0600

    KAN-185 Using a Map instead of an Array - Ensures Better scalability
    Added a map instead of an array of strings so now when we add more options for the avatar, all that is needed it to edit the size of the map, instead of adding the name of each item into the array.

--- App/Features/Profile/Avatar/AvatarModel.swift
+++ App/Features/Profile/Avatar/AvatarModel.swift
@@ -42,20 +42,20 @@ struct AvatarParts: Codable, Equatable {
 // Available avatar options
 struct AvatarOptions {
     // Personal avatar options (human)
-    static let personalBackgrounds = ["background_1", "background_2", "background_3", "background_4" , "background_5" , "background_6", "background_7", "background_8"]
-    static let personalBodies = ["body_1", "body_2", "body_3", "body_4", "body_5", "body_6", "body_7"]
-    static let personalShirts = ["shirt_1", "shirt_2", "shirt_3", "shirt_4", "shirt_5", "shirt_6", "shirt_7", "shirt_8", "shirt_9", "shirt_10", "shirt_11", "shirt_12", "shirt_13", "shirt_14", "shirt_15", "shirt_16"]
-    static let personalEyes = ["eyes_1", "eyes_2", "eyes_3", "eyes_4", "eyes_5", "eyes_6", "eyes_7"]
-    static let personalMouths = ["mouth_1", "mouth_2", "mouth_3", "mouth_4", "mouth_5", "mouth_6", "mouth_7"]
+    static let personalBackgrounds = (1...8).map { "background_\($0)" }
+    static let personalBodies = (1...7).map { "body_\($0)" }
+    static let personalShirts = (1...16).map { "shirt_\($0)" }
+    static let personalEyes = (1...7).map { "eyes_\($0)" }
+    static let personalMouths = (1...7).map { "mouth_\($0)" }
     static let personalHairs = (1...56).map { "hair_\($0)" } // hair one to 56, as an array
 
     
     // Fun avatar options (alien) - using actual fun_ prefixed files
-    static let funBackgrounds = ["background_1", "background_2", "background_3", "background_4" , "background_5" , "background_6", "background_7", "background_8"]
-    static let funFaces = ["fun_face_1", "fun_face_2", "fun_face_3", "fun_face_4", "fun_face_5", "fun_face_6", "fun_face_7", "fun_face_8", "fun_face_9", "fun_face_10", "fun_face_11", "fun_face_12", "fun_face_13", "fun_face_14", "fun_face_15", "fun_face_16"]
-    static let funEyes = ["fun_eyes_1", "fun_eyes_2", "fun_eyes_3", "fun_eyes_4", "fun_eyes_5", "fun_eyes_6", "fun_eyes_7", "fun_eyes_8", "fun_eyes_9", "fun_eyes_10"]
-    static let funMouths = ["fun_mouth_1", "fun_mouth_2", "fun_mouth_3", "fun_mouth_4", "fun_mouth_5", "fun_mouth_6", "fun_mouth_7", "fun_mouth_8", "fun_mouth_9", "fun_mouth_10", "fun_mouth_11"]
-    static let funHairs = ["fun_hair_1", "fun_hair_2", "fun_hair_3", "fun_hair_4", "fun_hair_5", "fun_hair_6"]
+    static let funBackgrounds = (1...8).map { "background_\($0)" }
+    static let funFaces = (1...16).map { "fun_face_\($0)" }
+    static let funEyes = (1...10).map { "fun_eyes_\($0)" }
+    static let funMouths = (1...11).map { "fun_mouth_\($0)" }
+    static let funHairs = (1...6).map { "fun_hair_\($0)" }
     
     // Legacy support - these will be deprecated
     static let backgrounds = personalBackgrounds
