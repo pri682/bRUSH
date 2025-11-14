c113d8152bd9e2c4cc4bbe5217ea8ed702a8e352
Meidad Troper <oro27@txstate.edu>
2025-11-13 19:25:10 -0600

    KAN-152 Added a New Unit Testing Target
    - Created a brand new Unit Testing Target in Xcode to allow all team members to add their tests.
    
    - Added iPhone and iPad as a target for the unit tests to allow running the tests in the simulator.

---
+++ UnitTests/UnitTests.swift
@@ -0,0 +1,16 @@
+//
+//  UnitTests.swift
+//  UnitTests
+//
+//  Created by Meidad Troper on 11/13/25.
+//
+
+import Testing
+
+struct UnitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
