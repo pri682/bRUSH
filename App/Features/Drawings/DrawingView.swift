fdddf0716f7be7e3500a829fc6a27d70ff573806
Vaidic Soni <wse11@txstate.edu>
2025-11-22 20:45:36 -0600

    Made changes to the way notification work KAN-165

--- App/Features/Drawings/DrawingView.swift
+++ App/Features/Drawings/DrawingView.swift
@@ -425,7 +425,7 @@ struct DrawingView: View {
         saveDrawingLocallyAndUpload()
 
         streakManager.markCompletedToday()
-        NotificationManager.shared.resetDailyReminders(hour: 20, minute: 0)
+        NotificationManager.shared.markTodayCompleted()
 
         withAnimation(.spring()) {
             showSubmittedPopup = true
